9b5730804146aa33caabac50a4b9bd4fc4e44d0e
Mihail <mishkagoo@gmail.com>
2021-08-31 00:26:57 +0300

    Adding timer and it animation to end of page. Counting time to  New Year

--- js/sctipt.js
+++ js/sctipt.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContetnt = document.querySelectorAll('.tabcontent'),
-          tabsParent  = document.querySelector('.tabheader__items');
 
-    function hideTabContent () {
+    //Tabs
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContetnt = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
         tabsContetnt.forEach(tab => {
-            tab.classList.add('hide');            
-            tab.classList.remove('show', 'fade');            
+            tab.classList.add('hide');
+            tab.classList.remove('show', 'fade');
         });
 
         tabs.forEach(tab => {
@@ -27,9 +29,9 @@ document.addEventListener('DOMContentLoaded', () => {
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
 
-        if (target && target.classList.contains('tabheader__item')){
+        if (target && target.classList.contains('tabheader__item')) {
             tabs.forEach((tab, i) => {
-                if(target == tab){
+                if (target == tab) {
                     hideTabContent();
                     showTabContent(i);
                 }
@@ -37,4 +39,59 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
+    //Timer
+
+    let deadline = '2022-1-1'; //Set timer
+
+    function getTimeRemaning(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function addZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        console.log(endtime);
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaning(endtime);
+
+            days.innerHTML = addZero(t.days);
+            hours.innerHTML = addZero(t.hours);
+            minutes.innerHTML = addZero(t.minutes);
+            seconds.innerHTML = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                const newTimer = `${new Date().getFullYear() + 1}-${new Date().getMonth()}-${new Date().getDate()}`;
+                setClock('.timer', newTimer);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
