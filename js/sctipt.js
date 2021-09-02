179aef7e1b1d81e53af0d39036ce627a9e52f3ca
Mihail <mishkagoo@gmail.com>
2021-09-02 23:55:40 +0300

    Some modern of MenuCard. Adding rest atributes

--- js/sctipt.js
+++ js/sctipt.js
@@ -149,12 +149,13 @@ document.addEventListener('DOMContentLoaded', () => {
     //Use classes for cards
 
     class MenuCard {
-        constructor(src, alt, name, discription, price, parentSelector) {
+        constructor(src, alt, name, discription, price, parentSelector, ...classes) {
             this.src = src;
             this.alt = alt;
             this.name = name;
             this.discription = discription;
             this.price = price;
+            this.classes = classes;
             this.parent = document.querySelector(parentSelector);
             this.transfer = 27;
             this.changeToUAH();
@@ -166,16 +167,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
         render() {
             const element = document.createElement('div');
+            if (this.classes.length === 0) {
+                this.element = 'menu__item';
+                element.classList.add(this.element);
+            } else {
+                this.classes.forEach(className => element.classList.add(className));
+            }
+
             element.innerHTML = `
-                <div class="menu__item">
-                    <img src=${this.src} alt=${this.alt}>
-                    <h3 class="menu__item-subtitle">${this.name}</h3>
-                    <div class="menu__item-descr">${this.discription}</div>
-                    <div class="menu__item-divider"></div>
-                    <div class="menu__item-price">
-                        <div class="menu__item-cost">Цена:</div>
-                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-                    </div>
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.name}</h3>
+                <div class="menu__item-descr">${this.discription}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
                 </div>
             `;
             this.parent.append(element);
